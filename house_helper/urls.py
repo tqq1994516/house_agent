e2b12a90047c9fada07875f28e197efd808d35ae
tianchenxu <472149011@qq.com>
2021-06-07 18:01:35 +0800

    修改login、register接口

--- house_helper/urls.py
+++ house_helper/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('message/', views.message, name='message'),
-    path('crm/', views.crm, name='crm'),
-    path('base_info/', views.base_info, name='base_info'),
+    # path('index/', views.index, name='index'),
+    # path('message/', views.message, name='message'),
+    # path('crm/', views.crm, name='crm'),
+    # path('base_info/', views.base_info, name='base_info'),
 ]
--- house_helper/views.py
+++ house_helper/views.py
@@ -24,32 +24,73 @@ class login(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        try:
-            if re.search('^\\d', request.data.get('username')).group():
-                userInfo = UserInfo.objects.get(mobile_phone=request.data.get('username')).first()
+        data = loginForm(data=request.data)
+        if data.is_valid():
+            try:
+                if re.search('^\\d', request.data.get('username')).group():
+                    userInfo = UserInfo.objects.get(mobile_phone=request.data.get('username')).first()
+                else:
+                    userInfo = UserInfo.objects.get(username=request.data.get('username')).first()
+                if userInfo['password'] == request.data.get('password') and userInfo['is_staff'] == 2 and userInfo[
+                    'is_active'] == 1:
+                    token = uuid4()
+                    cache.set(token, userInfo)
+                    UserInfo.objects.filter(id=userInfo['id']).update(is_active='1')
+                    response = myResponse(data={'message': '登录成功'})
+                    return Response(response, status=status.HTTP_200_OK)
+                else:
+                    response = myResponse(data={'message': '账号输入有误或密码错误', 'form': data})
+                    return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            except Exception:
+                response = myResponse(data={'message': '该账号未注册', 'form': data})
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class logout(APIView):
+    authentication_classes = []
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        pass
+
+
+class register(APIView):
+    authentication_classes = []
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        serializer = registerForm()
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = registerForm(data=request.data)
+        if data.is_valid():
+            valid_data = data.validated_data
+            repeatability_verification_username = UserInfo.objects.filter(username=valid_data['username']).first()
+            repeatability_verification_email = UserInfo.objects.filter(email=valid_data['email']).first()
+            repeatability_verification_mobile_phone = UserInfo.objects.filter(
+                mobile_phone=valid_data['mobile_phone']).first()
+            if repeatability_verification_username:
+                response = myResponse(data={'message': '账号已存在', 'form': data})
+            elif repeatability_verification_email:
+                response = myResponse(data={'message': '邮箱已存在', 'form': data})
+            elif repeatability_verification_mobile_phone:
+                response = myResponse(data={'message': '手机号已存在', 'form': data})
+            elif re.search('^\\d', valid_data['username']):
+                response = myResponse(data={'message': '账号不能以数字开头', 'form': data})
+            elif valid_data['password'] == valid_data['username']:
+                response = myResponse(data={'message': '密码不能与账号一致', 'form': data})
             else:
-                userInfo = UserInfo.objects.get(username=request.data.get('username')).first()
-            print(userInfo)
-        except Exception:
-            response = myResponse(data={'username': ['该账号未注册']})
+                try:
+                    data.save()
+                    response = myResponse(data={'message': '注册成功'})
+                    return Response(response, status=status.HTTP_200_OK)
+                except Exception as e:
+                    print(Exception, e)
+                    response = myResponse(data={'message': '注册信息表内已存在', 'form': data})
+                    return Response(response, status=status.HTTP_400_BAD_REQUEST)
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        # serializer = loginForm(data=request.data)
-        # if serializer.is_valid():
-        #     valid_result = serializer.save()
-        #     if re.search('^\\d', valid_result['username']).group():
-        #         verify_account = UserInfo.objects.get(Q(mobile_phone=valid_result['username']) | Q(username=valid_result['username']))
-        #         if verify_account.check_passord(valid_result['password']):
-        #             token = uuid4()
-        #             cache.set(token, verify_account)
-        #             response = myResponse(data={'login': ['登录成功']})
-        #             return Response(response, status=status.HTTP_200_OK)
-        #         else:
-        #             response = myResponse(data={'password': ['账号输入有误或密码错误']})
-        #     else:
-        #         response = myResponse(data={'username': ['该账号未注册']})
-        # else:
-        #     response = myResponse(data=serializer.errors)
-        # return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class menuViewSet(viewsets.ReadOnlyModelViewSet):
@@ -74,64 +115,16 @@ class menuViewSet(viewsets.ReadOnlyModelViewSet):
         response = myResponse(serializer.data, extra)
         return Response(response, status=status.HTTP_200_OK)
 
-
-class register(APIView):
-    authentication_classes = []
-    permission_classes = (permissions.AllowAny,)
-
-    def get(self, request):
-        serializer = registerForm()
-        return Response(serializer.data)
-
-    def post(self, request):
-        data = registerForm(data=request.data)
-        if data.is_valid():
-            valid_data = data.validated_data
-            repeatability_verification_username = UserInfo.objects.filter(username=valid_data['username']).first()
-            repeatability_verification_email = UserInfo.objects.filter(email=valid_data['email']).first()
-            repeatability_verification_mobile_phone = UserInfo.objects.filter(
-                mobile_phone=valid_data['mobile_phone']).first()
-            if repeatability_verification_username:
-                response = myResponse(data={'message': '账号已存在'})
-            elif repeatability_verification_email:
-                response = myResponse(data={'message': '邮箱已存在'})
-            elif repeatability_verification_mobile_phone:
-                response = myResponse(data={'message': '手机号已存在'})
-            elif re.search('^\\d', valid_data['username']):
-                response = myResponse(data={'message': '账号不能以数字开头'})
-            elif valid_data['password'] == valid_data['username']:
-                response = myResponse(data={'message': '密码不能与账号一致'})
-            else:
-                try:
-                    data.save()
-                    response = myResponse(data={'message': '注册成功'})
-                    return Response(response, status=status.HTTP_200_OK)
-                except Exception as e:
-                    print(Exception, e)
-                    response = myResponse(data={'message': '注册信息表内已存在'})
-                    return Response(response, status=status.HTTP_400_BAD_REQUEST)
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+# def index(request):
+#     if request.method == 'GET':
+#         this_fun_name = sys._getframe().f_code.co_name
+#         menu_dict = get_menu_list(this_fun_name)
+#         return render(request, 'index.html', {'menu_list': menu_dict, 'username': request.user})
 
 
-def index(request):
-    if request.method == 'GET':
-        this_fun_name = sys._getframe().f_code.co_name
-        menu_dict = get_menu_list(this_fun_name)
-        return render(request, 'index.html', {'menu_list': menu_dict, 'username': request.user})
-
-
-def crm(request):
-    return redirect('base_info')
-
-
-def base_info(request):
-    if request.method == 'GET':
-        this_fun_name = sys._getframe().f_code.co_name
-        menu_dict = get_menu_list(this_fun_name)
-        print(request.session)
-        return render(request, 'base_info.html', {'menu_list': menu_dict, 'username': request.user})
-
-
-def message(request):
-    pass
+# def base_info(request):
+#     if request.method == 'GET':
+#         this_fun_name = sys._getframe().f_code.co_name
+#         menu_dict = get_menu_list(this_fun_name)
+#         print(request.session)
+#         return render(request, 'base_info.html', {'menu_list': menu_dict, 'username': request.user})
